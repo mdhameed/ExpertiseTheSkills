eb6cb500fd7c8b4c7e27319624c3af7bff7f1fb6
Mohammed Hameed <hameed.in@hotmail.com>
2024-08-15 20:02:28 +0530

    Replace the output with Output decorator again.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,8 +11,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
- // @Output() select = new EventEmitter();
- select = output<string>();
+  @Output() select = new EventEmitter();
   
   get imagePath() {
     return 'assets/users/' + this.avatar;
