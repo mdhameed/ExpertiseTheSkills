8d82b802e70eb75012d81a6fbacf7579e3df6af4
Mohammed Hameed <hameed.in@hotmail.com>
2024-08-15 22:30:24 +0530

    Added Task Component and some styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find
-    ((user) => user.id === this.selectedUserId)!;
+    ((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
